8c16c8244d01271ab3ad34e0c9e441b56e756463
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-05-29 23:04:35 +0530

    added input validation Added headers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <algorithm>
 #include <numeric>
 #include <iomanip>
-using namespace std;
 int main() {
-            
+    std::string file_location;
+    std::cout<<"Enter the location of the .csv file : "<<std::endl;
+    std::cin>>file_location;
+
 }
