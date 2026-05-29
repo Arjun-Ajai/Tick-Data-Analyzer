c22bc5aeef969f3ae54aa4cb759573e33e435bae
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-05-29 23:15:48 +0530

    used fstream and sstream

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@
 #include <numeric>
 #include <iomanip>
 int main() {
-    std::string file_location;
+    std::string file_location,row,date,low,high,close,Volume;
     std::cout<<"Enter the location of the .csv file : "<<std::endl;
     std::cin>>file_location;
-
+    std::ifstream data_set(file_location);
+    if (data_set.is_open()) {
+        std::getline(data_set,row);
+        std::stringstream ss(row);
+    }
 }
