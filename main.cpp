6760ee38c3fab2fb57e4724d6e9d94d46547f82e
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-05-30 23:32:12 +0530

    refined file reader

--- main.cpp
+++ main.cpp
@@ -6,17 +6,31 @@
 #include <algorithm>
 #include <numeric>
 #include <iomanip>
+
+struct Tick {
+    std::string date;
+    double open;
+    double high;
+    double low;
+    double close;
+    double volume;
+};
+
 int main() {
     std::string file_location;
     std::cout<<"Enter the location of the CSV file : "<<std::endl;
     std::cin>>file_location;
     std::ifstream data_set(file_location);
+
     if (data_set.is_open()) {
         std::string row;
+        std::getline(data_set,row);
+        int i=0;
         while (std::getline(data_set,row))
-            std::cout<<row<<std::endl;
-        }
-    else {
+        std::stringstream row_stream(row);
+    }
+
+    else{
         std::cerr<<"Error! Could not open or find the file at "<<file_location<<std::endl;
     }
 
